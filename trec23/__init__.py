8d851be582dfba169922955a9e8083ba92339f27
Andrew Parry <2393265p@student.gla.ac.uk>
2023-07-19 11:15:54 +0100

    config load should not crash the script if no config defined

--- trec23/__init__.py
+++ trec23/__init__.py
@@ -1,8 +1,11 @@
 import os 
 import json 
 
-with open(os.getenv("TREC_CONFIG", "config.json"), 'r') as f:
-    try:
-        CONFIG = json.load(f)
-    except json.decoder.JSONDecodeError:
-        CONFIG = {}
+try:
+    with open(os.getenv("TREC_CONFIG", "config.json"), 'r') as f:
+        try:
+            CONFIG = json.load(f)
+        except json.decoder.JSONDecodeError:
+            CONFIG = {}
+except FileNotFoundError:
+    CONFIG = {}
