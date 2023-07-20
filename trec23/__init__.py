d917cb120b67b607cba5a4ea03820cc9df694e79
Andrew Parry <2393265p@student.gla.ac.uk>
2023-07-20 12:04:00 +0100

    previous commit made by idiot

--- trec23/__init__.py
+++ trec23/__init__.py
@@ -4,7 +4,7 @@ import json
 try:
     with open(os.getenv("TREC_CONFIG", "config.json"), 'r') as f:
         try:
-            CONFIG = json.load(open(f, 'r'))
+            CONFIG = json.load(f)
         except json.decoder.JSONDecodeError:
             CONFIG = {}
 except FileNotFoundError:
