6d689a87e89d3d68c6c823e5c7da791c028b199f
Andrew Parry <2393265p@student.gla.ac.uk>
2023-07-24 12:20:40 +0100

    force dir change for finding config.json

--- trec23/__init__.py
+++ trec23/__init__.py
@@ -1,14 +1,18 @@
 import os 
 import json 
-
+import logging
 try:
+    os.chdir('/')
     with open(os.getenv("TREC_CONFIG", "CONFIG.json"), 'r') as f:
         try:
             CONFIG = json.load(f)
         except json.decoder.JSONDecodeError:
+            logging.warning("CONFIG.json file found, but could not be parsed. Using default configuration.")
             CONFIG = {}
 except FileNotFoundError:
+    logging.warning("No CONFIG.json file found. Using default configuration.")
     CONFIG = {}
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     print(CONFIG)
