2c70df02172de337978edcc29c943d1772dc3e14
Andrew Parry <2393265p@student.gla.ac.uk>
2023-07-26 15:42:36 +0100

    idiot

--- trec23/runs/gar.py
+++ trec23/runs/gar.py
@@ -1,12 +1,10 @@
 from pyterrier_adaptive import GAR, CorpusGraph
-from trec23.pipelines.baselines import load_monot5
 from typing import Optional, Any
 
 def load_gar(model : Any, 
              dataset : str, 
              graph_variant : str,
              k : int = 8):
-    
     graph =  CorpusGraph.from_dataset(dataset, graph_variant).to_limit_k(k)
 
     return GAR(model, graph)
