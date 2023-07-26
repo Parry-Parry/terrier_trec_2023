e4cc98033aae6e5c563aa4138818aa5ad83e4e1a
Andrew Parry <2393265p@student.gla.ac.uk>
2023-07-26 16:15:07 +0100

    stop mental dependencies

--- trec23/runs/genqr.py
+++ trec23/runs/genqr.py
@@ -1,10 +1,10 @@
 from typing import Optional
-from pyterrier_generativeqr.transformer import GenerativeQR, GenerativePRF 
-from pyterrier_generativeqr.models import FLANt5
 
 def load_qr(llm_model_name_or_path : str, 
              llm_kwargs : Optional[dict] = {},
              **kwargs):
+    from pyterrier_generativeqr.transformer import GenerativeQR 
+    from pyterrier_generativeqr.models import FLANt5
     model = FLANt5(llm_model_name_or_path, **llm_kwargs)
     QR = GenerativeQR(model, **kwargs)
 
@@ -13,6 +13,8 @@ def load_qr(llm_model_name_or_path : str,
 def load_prf(llm_model_name_or_path : str, 
              llm_kwargs : Optional[dict] = {},
              **kwargs):
+    from pyterrier_generativeqr.transformer import GenerativePRF 
+    from pyterrier_generativeqr.models import FLANt5
     model = FLANt5(llm_model_name_or_path, **llm_kwargs)
     PRF = GenerativePRF(model, **kwargs)
 
