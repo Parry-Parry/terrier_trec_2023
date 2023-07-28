a104749837a9f42b69057232319f4d702fe3e549
Andrew Parry <2393265p@student.gla.ac.uk>
2023-07-28 11:22:49 +0100

    dont use pt get text twice

--- scripts/main/light/splade_gar_electra.py
+++ scripts/main/light/splade_gar_electra.py
@@ -24,7 +24,7 @@ def main(out_dir : str, irds : str = None, path : str = None, name : str = None,
     splade = trec23.load_splade(CONFIG['SPLADE_MARCOv2_PATH'], '/tmp/msmarco-passage-v2-dedup.splade.pisa', device=device)
     electra = pt.text.get_text(text_ref, 'text') >> trec23.load_electra(CONFIG['ELECTRA_MARCO_PATH'], device=device, batch_size=batch_size, verbose=False)
     gar = trec23.load_gar(electra, CONFIG['GAR_GRAPH_PATH'], num_results=budget, batch_size=batch_size, verbose=True)
-    model = splade % budget >> pt.text.get_text(text_ref, 'text') >> gar
+    model = splade % budget >> gar
 
     logging.info('Done.')
 
