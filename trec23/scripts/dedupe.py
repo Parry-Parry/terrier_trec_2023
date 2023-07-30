d93bc8754076ff486377482e20ccfb70659d1494
Andrew Parry <2393265p@student.gla.ac.uk>
2023-07-30 20:57:24 +0100

    fixed!

--- trec23/scripts/dedupe.py
+++ trec23/scripts/dedupe.py
@@ -1,7 +1,21 @@
-
+import pyterrier as pt
+pt.init()
 from fire import Fire
 import os
 from trec23 import CONFIG, MarcoDuplicator
+import json 
+import gzip 
 
-def main(path : str, out : str):
-    lookup = json.load(open(CONFIG['MARCOv2_DUPE_PATH'], 'r'))
+def dedupe_res(path : str, out : str):
+    lookup = json.load(gzip.open(CONFIG['MARCOv2_DUPE_PATH'], 'rb'))
+    res = pt.io.read_results(path)
+    dupe = MarcoDuplicator(lookup)
+
+    # group by qid, sort by rank and dedupe
+    cut = res.groupby('qid').apply(lambda x: x.sort_values('rank').head(100)).reset_index(drop=True)
+    new_res = dupe.transform(cut)
+
+    pt.io.write_results(new_res, out)
+
+if __name__ == '__main__':
+    Fire(dedupe_res)
