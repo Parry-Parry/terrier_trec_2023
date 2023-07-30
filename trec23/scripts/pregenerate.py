338859326c25a624b4f7bfecd4d84416a0a4dc95
Andrew Parry <2393265p@student.gla.ac.uk>
2023-07-30 22:16:48 +0100

    pre-compute script

--- trec23/scripts/pregenerate.py
+++ trec23/scripts/pregenerate.py
@@ -1,13 +1,25 @@
+import pyterrier as pt
+
 from fire import Fire
-import os
-from trec23 import CONFIG
-import ir_datasets as irds
+from trec23 import CONFIG, load_qr
 
 def expansion(
         irds : str = None,
         path : str = None,
+        out : str = None,
 ):
     assert irds is not None or path is not None, 'Either irds or path must be specified'
     if irds is not None:
-        dataset = irds.load(irds)
-        topics = dat
+        dataset = pt.get_dataset(irds)
+        topics = dataset.get_topics()
+    
+    if path is not None:
+        topics = pt.io.read_topics(path, format='singleline')
+
+    qr = load_qr(CONFIG['FLANT5_XXL_PATH'], llm_kwargs={'device_map' : 'sequential', 'load_in_8bit' : True, 'device' : 'cuda'})
+
+    expansions = qr.transform(topics)
+    expansions.to_csv(out, sep='\t', header=False, index=False)
+
+if __name__ == '__main__':
+    Fire(expansion)
