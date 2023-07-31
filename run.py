f46ae8fbe13eed9c253cc54c863a354f224a566c
Andrew Parry <2393265p@student.gla.ac.uk>
2023-07-31 16:33:20 +0100

    optional index copy

--- run.py
+++ run.py
@@ -5,24 +5,32 @@ import subprocess as sp
 import logging
 import trec23
 
-def main(script_dir : str, out_dir : str, irds : str = None, path : str = None, qrels=None, budget : int = 5000, script_name : str = None, batch_size : int = None, use_cache=False):
+def main(script_dir : str, 
+         out_dir : str, 
+         irds : str = None, 
+         path : str = None, 
+         qrels=None, 
+         budget : int = 5000, 
+         script_name : str = None, 
+         batch_size : int = None, 
+         use_cache=False,
+         no_dl : bool = False,):
     assert irds is not None or path is not None, 'Either irds or path must be specified'
     os.makedirs(out_dir, exist_ok=True)
 
     scripts = [f for f in os.listdir(script_dir) if not '__' in f]
 
     # check if director '/tmp/index.pisa' exists   
-    if not os.path.exists('/tmp/msmarco-passage-v2-dedup.pisa'):
-        logging.info('Copying PISA index...')
-        trec23.copy_index(path=trec23.CONFIG["PISA_PATH"])
-        logging.info('Done.')
+    if not no_dl:
+        if not os.path.exists('/tmp/msmarco-passage-v2-dedup.pisa'):
+            logging.info('Copying PISA index...')
+            trec23.copy_index(path=trec23.CONFIG["PISA_PATH"])
+            logging.info('Done.')
 
-    if not os.path.exists('/tmp/msmarco-passage-v2-dedup.splade.pisa'):
-        logging.info('Copying PISA SPLADE index...')
-        trec23.copy_index(path=trec23.CONFIG["PISA_SPLADE_PATH"])
-        logging.info('Done.')
-
-    trec23.copy_path(trec23.CONFIG["TERRIER_MARCOv2_PATH"])
+        if not os.path.exists('/tmp/msmarco-passage-v2-dedup.splade.pisa'):
+            logging.info('Copying PISA SPLADE index...')
+            trec23.copy_index(path=trec23.CONFIG["PISA_SPLADE_PATH"])
+            logging.info('Done.')
 
     if script_name is not None:
         scripts = [script_name]
