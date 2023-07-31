e80f771ed3d83767ae651efa2ffd095d27b78f9f
Andrew Parry <2393265p@student.gla.ac.uk>
2023-07-31 17:48:35 +0100

    adding tmp trace

--- scripts/main/run_all_heavy.py
+++ scripts/main/run_all_heavy.py
@@ -4,27 +4,29 @@ from os.path import join
 import logging
 import subprocess as sp
 
-irds = r'irds:msmarco-passage-v2/trec-dl-2022/judged'
-budget = '1000'
+#irds = r'irds:msmarco-passage-v2/trec-dl-2022/judged'
+#budget = '1000'
 DIR = r'/resources/terrier_trec_2023/scripts/main/'
-OUT_DIR = r'/resources/TREC23/eval/main/dl22'
 
 SUBDIRS = ['light', 'heavy/qr', 'heavy/prf']
 
-def main(use_cache=False):
+def main(out_dir : str, path : str = None, irds : str = None, budget : int = 1000, use_cache=False, qrels : str = None, no_dl : bool = False):
     for d in SUBDIRS:
         script_dir = join(DIR, d)
         scripts = [f for f in os.listdir(script_dir) if not '__' in f]
         for script in scripts:
             spath = join(script_dir, script)
             name = script.strip('.py')
-            if os.path.isdir(join(OUT_DIR, name)):
+            if os.path.isdir(join(out_dir, name)):
                 logging.info(f'Skipping {name} as it already exists.')
                 continue
-            args = f'python {spath} --out_dir {join(OUT_DIR, name)}  --name {name} --budget {budget}'
+            args = f'python {spath} --out_dir {join(out_dir, name)}  --name {name} --budget {budget}'
             args += f' --batch_size {64}'
-            args += f' --irds {irds}'
+            if irds: args += f' --irds {irds}'
+            if path: args += f' --path {path}'
             if use_cache: args += ' --use_cache'
+            if qrels: args += f' --qrels {qrels}'
+            if no_dl: args += ' --no_dl'
             
             logging.info(f'Running {args}')
             sp.run(args, shell=True)
--- trec23/runs/gar.py
+++ trec23/runs/gar.py
@@ -1,10 +1,12 @@
 from pyterrier_adaptive import GAR, CorpusGraph
 from typing import Optional, Any
+import pdb
 
 def load_gar(model : Any, 
              path : str, 
              k : int = None,
              **kwargs):
+    pdb.set_trace()
     graph =  CorpusGraph.load(path)
     if k is not None:
         graph = graph.to_limit(k)
