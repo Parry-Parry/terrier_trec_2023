7a32c34f3696a25c30aabe04ede7976617c9e494
Andrew Parry <2393265p@student.gla.ac.uk>
2023-07-31 17:56:11 +0100

    ahj

--- trec23/runs/gar.py
+++ trec23/runs/gar.py
@@ -1,12 +1,10 @@
 from pyterrier_adaptive import GAR, CorpusGraph
 from typing import Optional, Any
-import pdb
 
 def load_gar(model : Any, 
              path : str, 
              k : int = None,
              **kwargs):
-    pdb.set_trace()
     graph =  CorpusGraph.load(path)
     if k is not None:
         graph = graph.to_limit(k)
