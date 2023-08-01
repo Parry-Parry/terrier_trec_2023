ca7145c03a1ef3f019403afc58a330bf0ea9fdd4
Andrew Parry <2393265p@student.gla.ac.uk>
2023-08-01 09:23:25 +0100

    allow for cutting to original size

--- trec23/scripts/dedupe.py
+++ trec23/scripts/dedupe.py
@@ -7,7 +7,7 @@ import json
 import gzip 
 from os.path import join
 
-def dedupe_res(path : str, out : str, runname : str = None, cut : bool = False):
+def dedupe_res(path : str, out : str, runname : str = None, cut : bool = False, budget : int = 2000):
     lookup = json.load(gzip.open(CONFIG['MARCOv2_DUPE_PATH'], 'rb'))
     res = pt.io.read_results(path)
     dupe = MarcoDuplicator(lookup)
@@ -20,6 +20,7 @@ def dedupe_res(path : str, out : str, runname : str = None, cut : bool = False):
         res['run_name'] = runname
 
     if cut: res = res.groupby('qid').apply(lambda x: x.sort_values('rank').head(100)).reset_index(drop=True)
+    else: res = res.groupby('qid').apply(lambda x: x.sort_values('rank').head(budget)).reset_index(drop=True)
     res.sort_by(['qid', 'rank'], inplace=True)
     pt.io.write_results(res, join(out, f'deduped.{os.path.basename(path)}'), runname=runname)
 
