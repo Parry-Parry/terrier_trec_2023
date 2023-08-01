ddebed82fe828447c250baca773546f0cfb4f2ec
Andrew Parry <2393265p@student.gla.ac.uk>
2023-08-01 09:42:23 +0100

    fixed

--- trec23/scripts/dedupe.py
+++ trec23/scripts/dedupe.py
@@ -7,7 +7,7 @@ import json
 import gzip 
 from os.path import join
 
-def dedupe_res(path : str, out : str, runname : str = None, cut : bool = False, budget : int = 2000):
+def dedupe_res(path : str, out : str, runname : str = 'pyterrier', cut : bool = False, budget : int = 2000):
     lookup = json.load(gzip.open(CONFIG['MARCOv2_DUPE_PATH'], 'rb'))
     res = pt.io.read_results(path)
     dupe = MarcoDuplicator(lookup)
@@ -21,8 +21,8 @@ def dedupe_res(path : str, out : str, runname : str = None, cut : bool = False, 
 
     if cut: res = res.groupby('qid').apply(lambda x: x.sort_values('rank').head(100)).reset_index(drop=True)
     else: res = res.groupby('qid').apply(lambda x: x.sort_values('rank').head(budget)).reset_index(drop=True)
-    res.sort_by(['qid', 'rank'], inplace=True)
-    pt.io.write_results(res, join(out, f'deduped.{os.path.basename(path)}'), runname=runname)
+    res.sort_values(by=["qid", "score"], ascending=[True, False], inplace=True)
+    pt.io.write_results(res, join(out, f'{runname}.res.gz'), runname=runname)
 
 if __name__ == '__main__':
     Fire(dedupe_res)
